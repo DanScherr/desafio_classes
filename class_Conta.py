a210454cdf20094b17bc6c8975a39c24b770ced5
danielscheicher@gmail.com <danielscheicher@gmail.com>
2022-09-14 15:45:00 -0300

    Editing Daniel_Files

--- class_Conta.py
+++ class_Conta.py
@@ -9,11 +9,10 @@ from class_Depena_String import Depena_String
 # ------------ DEFINING CLASS ------------
 class Conta:
 
-# TODO: Criar construtor
 
+# TODO: Criar construtor
     def __init__(self, nome:str, cpf:str, senha:str, renda:float, chave_pix:str, saldo:float) -> None:
         '''Terá atributos do usuário como:'''
-
         self._nome      = self.set_nome(nome)
         self._cpf       = self.set_cpf(cpf)
         self._renda     = self.set_renda(renda)
@@ -25,14 +24,13 @@ class Conta:
 
 
 # TODO: Geters e Seters
-
     def get_nome(self) -> str:
         return self.nome
         pass
 
     def set_nome(self, nome) -> None:
         nome = Depena_String(nome)
-        self.nome = nome.string
+        self.nome = nome.nomeComposto()
         pass
 
     def get_cpf(self) -> str:
@@ -59,7 +57,7 @@ class Conta:
         self.senha = senha
         pass
 
-    def get_set_chave_pix(self) -> str:
+    def get_chave_pix(self) -> str:
         return self.chave_pix
         pass
 
@@ -75,8 +73,8 @@ class Conta:
         self.saldo = saldo
         pass
 
-# TODO: Métodos da classe conta
 
+# TODO: Métodos da classe conta
     def criar_cartao_de_credito(self) -> CartaoDeCredito:
         '''Criar objeto cartao de credito na conta, dados limite e nome do cartão de crédito, chamará metodo de análise de crédito do banco
         para definir limite'''
@@ -84,13 +82,22 @@ class Conta:
         self.lista_cartoes_de_credito.append(novo_cartao_de_credito)
         pass
 
-    def pagar_boleto(self) -> bool:
+    def pagar_boleto(self, valor: float) -> bool:
         '''Pagar boleto dado valor'''
+        # Se saldo corrente é menor que a qtd desejada de sacar, retorna false
+        if (( self.get_saldo() < valor ) and
+            ( type(valor) != float )):
+            return False
+        # Caso contrário subtrai o valor da conta e retorna true
+        else:
+            self.set_saldo(self.get_saldo() - valor)
+            return True
         pass
     
     def sacar_dinheiro(self, qtd:float) -> bool:
         # Se saldo corrente é menor que a qtd desejada de sacar, retorna false
-        if self.get_saldo() < qtd:
+        if (( self.get_saldo() < qtd ) and 
+            ( type(qtd) != float )):
             return False
         # Caso contrário subtrai o valor da conta e retorna true
         else:
@@ -99,12 +106,28 @@ class Conta:
         pass 
 
     def depositar_dinheiro(self, qtd:float) -> bool:
+        # Se saldo corrente é menor que a qtd desejada de depositar, retorna false
+        if (type(qtd) != float):
+            return False
+        # Caso contrário subtrai o valor da conta e retorna true
+        else:
+            self.set_saldo(self.get_saldo() + qtd)
+            return True
         pass
 
-    def criar_alterar_chave_pix(self, chave_pix:str) -> bool:
+    def criar_alterar_chave_pix(self, nova_chave_pix:str) -> bool:
+        if ( type(nova_chave_pix) == str ):
+            self.set_chave_pix(nova_chave_pix)
         pass
 
     def fazer_pix(self, chave_pix_destino:str, qtd:float, banco_destino:Banco) -> bool:
         '''Dada um objeto de conta origem, e uma chave pix destino, e quantidade da transferencia, fazer redução no saldo de uma conta e aumento de saldo na conta destino'''
+        for conta in banco_destino.lista_clientes:
+            if (( conta.get_chave_pix() != chave_pix_destino ) or
+                ( self.get_saldo() < qtd )):
+                return False
+            else:
+                self.set_saldo(self.get_saldo() - qtd)
+                conta.set_saldo(qtd)
+                return True
         pass
-
--- class_Depena_String.py
+++ class_Depena_String.py
@@ -1,26 +1,26 @@
 # ------------ DEFINING CLASS ------------
-from multiprocessing.sharedctypes import Value
-
 
 from re import search
 
+
 class Depena_String:
 
-    # TODO: Criar construtor
 
+    # TODO: Criar construtor
     def __init__(self, string:str) -> None:
-        self._string = self.del_empty_spaces(string)
+        self._string = self.set_string(string)
         pass
 
-
-    # TODO: Metodo que trata nomes compostos (Deleta nomes vazios e Deixa a primeira leta de cada nome maíuscula e o resto minúscula)
-
-    def del_empty_spaces(self, string: str):
-        name_special_chars = '^[a-zA-Z]*$'
-        if type(string) == str:
-            # s_no_space = [value.lower().capitalize() for value in string.split(' ') if ( (value != '') and (not value.isnumeric()) )]
-            s_no_space = [value.lower().capitalize() for value in string.split(' ') if ( (value != '') and (bool((search(name_special_chars ,value)))) )]
-            s_no_space = ' '.join(s_no_space)
-            self.string = s_no_space
+    def set_string(self, string: str):
+        if (type(string) == str):
+            self.string = string
         else:
             raise Exception("\n - ERRO1: Digite um texto.")
+
+    # TODO: Metodo que trata nomes compostos (Deleta nomes vazios e Deixa a primeira leta de cada nome maíuscula e o resto minúscula)
+    def nomeComposto(self) -> str:
+        name_special_chars = '^[a-zA-Z]*$'
+        # s_no_space = [value.lower().capitalize() for value in string.split(' ') if ( (value != '') and (not value.isnumeric()) )]
+        nomeComposto = [value.lower().capitalize() for value in self.string.split(' ') if ( (value != '') and (bool((search(name_special_chars ,value)))) )]
+        nomeComposto = ' '.join(nomeComposto)
+        return nomeComposto
