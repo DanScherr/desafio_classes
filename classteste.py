7a1d5a972207572fd49d478d39c9dc331b3d9323
hara0-tavares <94493864+hara0-tavares@users.noreply.github.com>
2022-09-16 15:45:58 -0300

    Comentando o codigo e fazendo ultimos ajustes.

--- classteste.py
+++ classteste.py
@@ -35,11 +35,17 @@ class Banco:
             
 
     def analisar_credito(self, cpf:str):
+        '''Dada renda de uma conta, definir regras que retornem um limite adequado para o cliente'''
+        # Primeiro: percorrer lista de clientes do banco
         for i in self.lista_clientes:
+            # Segundo: comparar parametros
             if i.get_cpf() == cpf:
                 if i.renda >= 2000:
+                    #Multiplicar a renda por um valor para chegar ao limite de credito estabecido
                     limite_cartao = i.renda * 0.3
+                    #Ira retornar o valor do limite do cartao de acordo com a funcao
                     return limite_cartao
+                #Caso a renda seja menor, ira fazer um calculo diferente, dando um limite mais baixo ao cliente
                 else: 
                     limite_cartao = i.renda * 0.1
                     return limite_cartao
